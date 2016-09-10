86b450bd35243d02f784a036a43837d4c288b482
renzon <renzon@gmail.com>
2016-09-10 11:36:18 -0300

    contagem de letras com defaultdict

--- contador_letras.py
+++ contador_letras.py
@@ -1,8 +1,15 @@
-histograma = {}
+from collections import defaultdict
+
+
+def zero_factory():
+    return 0
+
+
+histograma = defaultdict(zero_factory)
 
 letras = 'abcajhgadsligads'
 
 for char in letras:
-    histograma[char] = histograma.get(char, 0) + 1
+    histograma[char] += 1
 
 print(histograma)
